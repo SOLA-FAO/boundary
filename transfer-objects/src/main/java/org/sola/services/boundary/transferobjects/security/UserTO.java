c44a6829b6cb07c9673c2770f0099773a1d5dee1
amcdowell <armcdowell@hotmail.com>
2013-09-06 13:45:20 +1200

    Enforce User Password Expiry [#326 responsible:none state:fixed milestone:v2.0]
    Added password expiry functionality. The system.appuser config in the SolaRealm
    of Glassfish should be changed to reference the system.active_users view. This
    view implements functionality to support password expiry from the JDBCRealm.
    
    The length of time a users password remains valid is controlled by the
    pword-expiry-days system.setting. Default is 90 days. If this setting is
    NULL or disabled, no password expiry is used.
    
    Users will be warned if their password is within 14 days of expiry and
    prompted to renew it. For most users, once their password expires, they
    will no longer be able to login to SOLA. Users with the ManageSecurity
    and/or NoPasswordExpiry role will still be able to login to SOLA once
    thier password is expired. (i.e. Admin users and user accounts required
    for external systems integration with SOLA web services.)
    
    Users will also be prompted to change their password if they were not
    the last person to set it (i.e. to cover situations where an Admin
    resets a users password).

--- transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/security/UserTO.java
+++ transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/security/UserTO.java
@@ -1,40 +1,46 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.services.boundary.transferobjects.security;
 
+import java.util.Date;
 import java.util.List;
 
 public class UserTO extends UserSummaryTO {
+
     private String userName;
     private String pasword;
     private List<UserGroupTO> userGroups;
     private boolean active;
     private String password;
+    private String lastPwordChangeUser;
+    private Integer pwordExpiryDays;
 
     public String getPassword() {
         return password;
@@ -44,7 +50,6 @@ public class UserTO extends UserSummaryTO {
         this.password = password;
     }
 
-    
     public UserTO() {
         super();
     }
@@ -64,7 +69,7 @@ public class UserTO extends UserSummaryTO {
     public void setPasword(String pasword) {
         this.pasword = pasword;
     }
-    
+
     public String getUserName() {
         return userName;
     }
@@ -72,7 +77,7 @@ public class UserTO extends UserSummaryTO {
     public void setUserName(String userName) {
         this.userName = userName;
     }
-    
+
     public boolean isActive() {
         return active;
     }
@@ -80,4 +85,21 @@ public class UserTO extends UserSummaryTO {
     public void setActive(boolean active) {
         this.active = active;
     }
+
+    public String getLastPwordChangeUser() {
+        return lastPwordChangeUser;
+    }
+
+    public void setLastPwordChangeUser(String lastPwordChangeUser) {
+        this.lastPwordChangeUser = lastPwordChangeUser;
+    }
+
+    public Integer getPwordExpiryDays() {
+        return pwordExpiryDays;
+    }
+
+    public void setPwordExpiryDays(Integer pwordExpiryDays) {
+        this.pwordExpiryDays = pwordExpiryDays;
+    }
+    
 }
